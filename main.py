990f208dc30024661f1ac89f40205da29644bd4c
crybaby-afk <toolarry55@gmail.com>
2025-01-18 16:31:32 +0300

    updated

--- main.py
+++ main.py
@@ -1,12 +1,28 @@
 class Customer:
     def __init__(self,name):
        self.name=self.validate_name(name)
+       self.orders = []
 
     def validate_name(self,name):
         if isinstance(name,str)and 15>= len(name) >=1 :
             return name
         else:
             raise ValueError("Unavailable")
+        
+        
+    def orders(self):
+        return set([order for order in Order.all if order.coffee==self])
+    
+    def customers(self):
+        return  set([order for order in order.all if order.customer==self])
+    
+
+def create_order(self,coffee,price):
+    if isinstance (coffee,Coffee) and isinstance(price,float):
+         new_Order=(price,self,coffee)
+         self.orders.append()
+         return new_Order
+    
         
     
 
@@ -33,10 +49,62 @@ class Coffee:
         """Returns the coffee's name."""
         return self._name
     
+    def orders(self):
+        return set([order for order in Order.all if order.coffee==self])
+    
+    def customers(self):
+        return set([order for order in order.all if order.customer==self])
+    
+
+    def num_orders
+
+
+
+class Order:
+    all=[]
+    def __init__(self,price,customer=None, coffee=None):
+        if hasattr(self, "_price"):
+            raise AttributeError("")
+
+        self.customer = self.validate_customer(customer)
+        self.coffee = self.validate_coffee(coffee)
+        self._price = self.validate_price(price)
+        Order.all.append(self)
+
+
+
+    def validate_price(price):
+        #to validate price
+        if isinstance(price, float) and 1.0 <= price <= 10.0:
+            return price
+        else:
+            raise ValueError("invalid price")
+        
+
+    def customer(self,customer):
+        if isinstance (customer,Customer):
+            return customer
+        else:
+            raise ValueError("Customer is invalid")
+        
+
+    
+    def Coffee(self,Coffee):
+        if isinstance (Coffee,Coffee):
+            return Coffee
+        else:
+            raise ValueError("Coffee is invalid")
+            
+
+    
+    
+
+
+    
         
 
 
 
-coffee=Coffee("mocha")
-coffee.name = ("chai moto")
-print (coffee.name)
+order=Order(10.0)
+order.price = ()
+print (order.price)
